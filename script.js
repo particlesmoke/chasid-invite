90a8dc3219ba84fed7f6cd13c864d6189af20b42
particlesmoke <f20180468@pilani.bits-pilani.ac.in>
2022-11-06 22:01:43 +0530

    dir change

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const themeColors = {
 	mehendi:'#50a80d',
 	home:'#ffe373'
 }
+const dropIntervals = []
 
 var parallaxInstance = new Parallax(scene,{
 	limitX:50,
@@ -24,6 +25,7 @@ new fullpage('#fullpage', {
 		// if(origin=='home')
 		// scene.style.top='50px'
 		document.getElementsByTagName('audio')[0].play()
+		dropIntervals.forEach(interval=>clearInterval(interval))
 		if(destination.anchor=='mehendi' || destination.anchor=='haldi' || destination.anchor=='wedding') 
 		{
 			let textElements = document.getElementById(`${destination.anchor}-invite`).getElementsByClassName('content')[0].children
@@ -31,9 +33,18 @@ new fullpage('#fullpage', {
 				textElements[x].className+=' focus-in-contract-bck'
 			}
 			if(destination.anchor=='haldi'){
-				setInterval(()=>{
-					dropOneFoolInHaldi()
-				},3000)
+				dropIntervals.push(
+					setInterval(()=>{
+						dropOneFoolInHaldi()
+					},1500)
+				)
+			}
+			if(destination.anchor=='wedding'){
+				dropIntervals.push(
+					setInterval(()=>{
+						dropOnePetalInWedding()
+					},250)
+				)
 			}
 
 		}
@@ -44,6 +55,17 @@ new fullpage('#fullpage', {
 })
 
 window.addEventListener('load',()=>{
+	// fadeOut(
+	// 	document.getElementById('loading-screen'),500
+	// )
+	fadeOut(document.getElementsByClassName('dank-ass-loader')[0],500)
+	setTimeout(()=>{
+		fadeIn(document.getElementById('CS-logo-loading'),800)
+		fadeIn(document.getElementById('open-invite-button'), 1200)
+	},500)
+})
+
+document.getElementById('open-invite-button').addEventListener('click', ()=>{
 	fadeOut(
 		document.getElementById('loading-screen'),500
 	)
@@ -56,6 +78,21 @@ function fadeOut( elem, ms )
     var timer = setInterval( function() {
 		opacity -= 30 / ms;
 		if( opacity <= 0 ){
+			clearInterval(timer);
+			opacity = 0;
+			elem.style.display = "none";
+			elem.style.visibility = "hidden";
+		}
+		elem.style.opacity = opacity;
+		elem.style.filter = "alpha(opacity=" + opacity * 100 + ")";
+    }, 30 ); 
+}
+function fadeIn( elem, ms )
+{
+    var opacity = 0;
+    var timer = setInterval( function() {
+		opacity += 30 / ms;
+		if( opacity >= 100 ){
 			clearInterval(timer);
 			opacity = 0;
 			elem.style.display = "none";
@@ -81,5 +118,24 @@ function dropOneFoolInHaldi(){
 			left-= (Math.random())*85
 	}
 	img.style.left = `${left}vw`
-	box.appendChild(img)
+	setTimeout(()=>box.appendChild(img), Math.random()*2000)
+}
+
+var petalDropCount = 1
+function dropOnePetalInWedding(){
+	const box = document.getElementById(`wedding-invite`).getElementsByClassName('droppings')[0]
+	const img = document.createElement('img')
+	img.src = `../Assets/p${petalDropCount%4 +1}.png`
+	img.className = 'petal'
+	let left = Math.random()*155
+	// if (left>0.3&&left<0.55){
+	// 	if(left>0.42)
+	// 		left+= (Math.random())*85
+	// 	else
+	// 		left-= (Math.random())*85
+	// }
+	img.style.left = `${left}vw`
+	img.style.animationName = `falling${petalDropCount%3 +1}`
+	petalDropCount++
+	setTimeout(()=>box.appendChild(img), Math.random()*2000)
 }
